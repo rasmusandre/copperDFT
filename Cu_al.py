1b04df41bb0b04e2507f4298b3b5163c3a497861
rasmusandre <rasmusandre@gmail.com>
2018-09-13 10:32:56 +0200

    Third commit

--- Cu_al.py
+++ Cu_al.py
@@ -5,7 +5,7 @@ from ase.visualize import view
 from gpaw import GPAW, PW, FermiDirac
 import matplotlib.pyplot as plt
 
-def the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear):
+def the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear, is_lat_const):
 
     #example 2, 10, 1, False, True, False
 
@@ -17,6 +17,9 @@ def the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_k
         name = 'Cu-fcc'
         a = 3.62  # fcc lattice parameter Cu [Å]
         b = a / 2
+
+        if is_lat_const:
+            b = k/200 # divide by 2 and by 100
 
         bulk = Atoms('Cu',cell=[[0, b, b],[b, 0, b],[b, b, 0]], pbc=True)
 
@@ -31,20 +34,27 @@ def the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_k
             k = k/100
             calc = GPAW(mode=PW(340), nbands = -2, xc='PBE', kpts=(8, 8, 8), occupations=FermiDirac(k),
                         txt=name + '.txt')
+        if is_lat_const:
+            calc = GPAW(mode=PW(340), nbands = -2, xc='PBE', kpts=(8, 8, 8), occupations=FermiDirac(0.1),
+                        txt=name + '.txt')
 
         bulk.set_calculator(calc)
 
         energy = bulk.get_potential_energy()
         calc.write(name + '.gpw')
         print('Energy:', energy, 'eV')
-        changing_parameter.append(k)
+        if is_lat_const:
+            changing_parameter.append(b*2)
+        else:
+            changing_parameter.append(k)
         energies.append(energy)
-    view(bulk)
+
+    #view(bulk)
     return changing_parameter, energies
 
-def plot_changing_param_vs_energy(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear):
+def plot_changing_param_vs_energy(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear, is_lat_const):
 
-    changing_parameter, energies = the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear)
+    changing_parameter, energies = the_parameter_changer(start_iteration, end_iteration, increment, is_ec, is_kpts, is_smear, is_lat_const)
 
     if is_kpts:
         changing_parameter = [i**3 for i in changing_parameter]
@@ -53,16 +63,23 @@ def plot_changing_param_vs_energy(start_iteration, end_iteration, increment, is_
         x_label = "E_cutoff"
     if is_smear:
         x_label = "K_BT"
-
+    if is_lat_const:
+        x_label = "Lattice constant"
 
     my_fig = plt.figure(0)
-    plt.semilogx(changing_parameter, energies)
-    plt.semilogx(changing_parameter, energies,'*')
+
+    if not is_lat_const:
+        plt.semilogx(changing_parameter, energies)
+        plt.semilogx(changing_parameter, energies,'*')
+    else:
+        plt.plot(changing_parameter, energies)
+        plt.plot(changing_parameter, energies,'*')
+
     plt.xlabel(x_label)
     plt.ylabel('Energy, eV')
     my_fig.savefig(x_label, bbox_inches='tight')
-    #plt.show()
+    plt.show()
 
-plot_changing_param_vs_energy(200,1200,100,True,False,False)
-#plot_changing_param_vs_energy(2,12,1,False,True,False)
+plot_changing_param_vs_energy(350,380,5,False,False,False,True)
+#plot_changing_param_vs_energy(2,12,1,False,True,False,False)
 #plot_changing_param_vs_energy(1,100,15,False,False,True)
